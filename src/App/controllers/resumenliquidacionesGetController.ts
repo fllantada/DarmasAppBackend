a13e230c4f38f1563bd354ff108c672d0c3b6489
Francisco Llantada <franciscollantada@gmail.com>
2022-08-18 21:03:05 -0300

    OTro bug corregido

--- src/App/controllers/resumenliquidacionesGetController.ts
+++ src/App/controllers/resumenliquidacionesGetController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { LiquidacionesSemanales } from '../../Context.liquidaciones/Liquidaciones/aplication/LiquidacionesSemanales';
 import { Controller } from '../shared/interfaces/Controller';
 import { DentalinkRepository } from '../../Context.liquidaciones/Liquidaciones/infrastructure/dentalinkRepository';
+import { MongoRepository } from '../../Context.liquidaciones/Liquidaciones/infrastructure/MongoRepository';
 export class ResumenLiquidacionesGetController implements Controller {
   constructor() {}
   async run(req: Request, res: Response): Promise<void> {
@@ -9,15 +10,17 @@ export class ResumenLiquidacionesGetController implements Controller {
     console.log('---Inicie resumen liquiudaciones get controller');
 
     //llama a la aplicacion
-    const repository = new DentalinkRepository();
-    const liquidaciones = new LiquidacionesSemanales(repository);
+    const dentalinkRepository = new DentalinkRepository();
+    const mongoRepository = new MongoRepository();
+    const liquidaciones = new LiquidacionesSemanales(dentalinkRepository, mongoRepository).run();
     //gestiona servicios, modelos, repsositorios, etc
-    liquidaciones.run();
+
     //tengo q pasarle los metodos que necesita de infraestructura.
 
     //Le tengo q psar un repositorio
 
     //Enviar respuesta
+    console.log('Por devolver Liquidaciones', liquidaciones);
 
     res.send('ok');
   }
